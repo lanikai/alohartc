b61cc0cb3cf42a3d48bd123fb2e21493f6e0ebae
Chris Hiszpanski <chris@lanikailabs.com>
2018-07-16 19:18:08 -0700

    Working stun binding request

--- peer_connection.go
+++ peer_connection.go
@@ -1,10 +1,23 @@
 package webrtc
 
 import (
+	"bufio"
 	"log"
+	"net"
+	"strings"
 )
 
 type PeerConnection struct {
+	// Connection to peer. May be over TCP or UDP.
+	conn net.Conn
+
+	// Local session description
+	localDescription string
+
+	// Remote peer session description
+	remoteDescription string
+
+	password string
 }
 
 func NewPeerConnection() *PeerConnection {
@@ -16,13 +29,13 @@ func NewPeerConnection() *PeerConnection {
 // Add remote ICE candidate
 func (pc *PeerConnection) AddIceCandidate(candidate string) error {
 	// STUN binding request
-	log.Println("send stun binding request")
+	StunBindingRequest(candidate, pc.password)
 
 	// STUN binding success response
-	log.Println("send stun binding request")
+//	log.Println("send stun binding request")
 
 	// DTLS client hello
-	log.Println("send stun binding request")
+//	log.Println("send stun binding request")
 
 	return nil
 }
@@ -34,5 +47,15 @@ func (pc *PeerConnection) CreateAnswer() (string, error) {
 
 // Set remote SDP offer
 func (pc *PeerConnection) SetRemoteDescription(sdp string) error {
+	pc.remoteDescription = sdp
+
+	scanner := bufio.NewScanner(strings.NewReader(sdp))
+	for scanner.Scan() {
+		line := scanner.Text()
+		if strings.HasPrefix(line, "a=ice-pwd") {
+			pc.password = strings.Split(line, ":")[1]
+			log.Println(pc.password)
+		}
+	}
 	return nil
 }
