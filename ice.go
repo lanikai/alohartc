3a2178dbe7bbcb3a1b8e5d0a4a188b0e950e31aa
Jordan Carlson <jwgcarlson@gmail.com>
2018-08-05 03:27:12 +0000

    More progress toward full STUN request/response exchange.
    Plus miscellaneous cleanups.

--- ice.go
+++ ice.go
@@ -43,41 +43,16 @@ func (agent *IceAgent) AddRemoteCandidate(desc string) error {
 	return nil
 }
 
-func (agent *IceAgent) GatherCandidates() ([]IceCandidate, error) {
-	// Listen on an arbitrary UDP port.
-	conn, err := net.ListenPacket("udp4", ":0")
-	if err != nil {
-		return nil, err
+func (c IceCandidate) String() string {
+	var b strings.Builder
+	b.WriteString(
+		fmt.Sprintf("candidate:%s %d %s %d %s %d",
+		c.foundation, c.component, c.protocol, c.priority, c.ip, c.port))
+	for _, key := range c.attrkeys {
+		val := c.attrs[key]
+		b.WriteString(" " + key + " " + val)
 	}
-	localAddr := conn.LocalAddr().(*net.UDPAddr)
-	log.Println("Listening on UDP", localAddr)
-
-	c := IceCandidate{
-		foundation: "0",
-		component: 1,
-		protocol: "udp",
-		priority: 100,
-		ip: localAddr.IP.String(),
-		port: localAddr.Port,
-	}
-	c.setAttr("typ", "host")
-
-	// TODO: Query public STUN server to get server reflexive candidates.
-	c2 := IceCandidate{
-		foundation: "0",
-		component: 1,
-		protocol: "udp",
-		priority: 99,
-		ip: "127.0.0.1",
-		port: localAddr.Port,
-	}
-	c2.setAttr("typ", "host")
-
-	agent.localAddr = localAddr
-	agent.conn = conn
-	agent.localCandidates = append(agent.localCandidates, c, c2)
-
-	return agent.localCandidates, nil
+	return b.String()
 }
 
 func parseCandidate(desc string) (IceCandidate, error) {
@@ -107,14 +82,84 @@ func (c *IceCandidate) setAttr(key string, val string) {
 	c.attrkeys = append(c.attrkeys, key)
 }
 
-func (c IceCandidate) String() string {
-	var b strings.Builder
-	b.WriteString(
-		fmt.Sprintf("candidate:%s %d %s %d %s %d",
-		c.foundation, c.component, c.protocol, c.priority, c.ip, c.port))
-	for _, key := range c.attrkeys {
-		val := c.attrs[key]
-		b.WriteString(" " + key + " " + val)
+func (agent *IceAgent) GatherCandidates() ([]IceCandidate, error) {
+	// Listen on an arbitrary UDP port.
+	conn, err := net.ListenPacket("udp4", ":0")
+	if err != nil {
+		return nil, err
 	}
-	return b.String()
+	localAddr := conn.LocalAddr().(*net.UDPAddr)
+	log.Println("Listening on UDP", localAddr)
+
+	lc := IceCandidate{
+		foundation: "0",
+		component: 1,
+		protocol: "udp",
+		priority: 100,
+		ip: localAddr.IP.String(),
+		port: localAddr.Port,
+	}
+	lc.setAttr("typ", "host")
+
+	// TODO: Query public STUN server to get server reflexive candidates.
+	sc := IceCandidate{
+		foundation: "1",
+		component: 1,
+		protocol: "udp",
+		priority: 50,
+		ip: "35.206.106.139",
+		port: localAddr.Port,
+	}
+	sc.setAttr("typ", "host")
+
+	agent.conn = conn
+	agent.localAddr = localAddr
+	agent.localCandidates = []IceCandidate{ lc, sc }
+
+	return agent.localCandidates, nil
+}
+
+// Send a STUN binding request to the given address, and await a binding response.
+func stunBindingExchange(conn net.PacketConn, hostPort string) (*IceCandidate, error) {
+	addr, err := net.ResolveUDPAddr("udp", hostPort)
+	if err != nil {
+		return nil, err
+	}
+
+	req := newStunBindingRequest()
+	_, err = conn.WriteTo(req.Bytes(), addr)
+	if err != nil {
+		log.Println("Failed to send STUN binding request:", err)
+		return nil, err
+	}
+	log.Println("Sent STUN binding request")
+
+	buf := make([]byte, 1500)
+	n, _, err := conn.ReadFrom(buf)
+	if err != nil {
+		log.Println("Did not receive STUN binding response:", err)
+		return nil, err
+	}
+	log.Println("Received STUN binding response")
+
+	resp := parseStunMessage(buf[:n])
+	if resp == nil {
+		return nil, fmt.Errorf("STUN binding response is invalid")
+	}
+
+	if resp.header.TransactionID != req.header.TransactionID {
+		return nil, fmt.Errorf("Unknown transaction ID in STUN binding response: %s", resp.header.TransactionID)
+	}
+	if resp.class != stunSuccessResponseClass {
+		return nil, fmt.Errorf("STUN binding response is not successful: %d", resp.class)
+	}
+
+	// Find XOR-MAPPED-ADDRESS attribute in the response.
+
+
+	return nil, nil
+}
+
+func (agent *IceAgent) CheckConnectivity() error {
+	return nil
 }
