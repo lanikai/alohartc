315dd8587d941d1465c93f1106ff0134903c06c3
Chris Hiszpanski <chris@lanikailabs.com>
2018-09-06 17:12:09 -0700

    Update peer_connection.go
    Support only baseline H.264 profile. No support for retransmissions or RTP headers.

--- peer_connection.go
+++ peer_connection.go
@@ -50,7 +50,7 @@ s=-
 t=0 0
 a=group:BUNDLE video
 a=msid-semantic: WMS SdWLKyaNRoUSWQ7BzkKGcbCWcuV7rScYxCAv
-m=video 9 UDP/TLS/RTP/SAVPF 96 97 98 99 100 101 102 123 127 122 125 107 108 109 124
+m=video 9 UDP/TLS/RTP/SAVPF 100
 c=IN IP4 0.0.0.0
 a=rtcp:9 IN IP4 0.0.0.0
 a=ice-ufrag:n3E3
@@ -63,45 +63,7 @@ a=sendonly
 a=rtcp-mux
 a=rtcp-rsize
 a=rtpmap:100 H264/90000
-a=rtcp-fb:100 goog-remb
-a=rtcp-fb:100 transport-cc
-a=rtcp-fb:100 ccm fir
-a=rtcp-fb:100 nack
-a=rtcp-fb:100 nack pli
-a=fmtp:100 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42001f
-a=rtpmap:101 rtx/90000
-a=fmtp:101 apt=100
-a=rtpmap:102 H264/90000
-a=rtcp-fb:102 goog-remb
-a=rtcp-fb:102 transport-cc
-a=rtcp-fb:102 ccm fir
-a=rtcp-fb:102 nack
-a=rtcp-fb:102 nack pli
-a=fmtp:102 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=42e01f
-a=rtpmap:123 rtx/90000
-a=fmtp:123 apt=102
-a=rtpmap:127 H264/90000
-a=rtcp-fb:127 goog-remb
-a=rtcp-fb:127 transport-cc
-a=rtcp-fb:127 ccm fir
-a=rtcp-fb:127 nack
-a=rtcp-fb:127 nack pli
-a=fmtp:127 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=4d0032
-a=rtpmap:122 rtx/90000
-a=fmtp:122 apt=127
-a=rtpmap:125 H264/90000
-a=rtcp-fb:125 goog-remb
-a=rtcp-fb:125 transport-cc
-a=rtcp-fb:125 ccm fir
-a=rtcp-fb:125 nack
-a=rtcp-fb:125 nack pli
-a=fmtp:125 level-asymmetry-allowed=1;packetization-mode=1;profile-level-id=640032
-a=rtpmap:107 rtx/90000
-a=fmtp:107 apt=125
-a=rtpmap:108 red/90000
-a=rtpmap:109 rtx/90000
-a=fmtp:109 apt=108
-a=rtpmap:124 ulpfec/90000
+a=fmtp:100 level-asymmetry-allowed=1;packetization-mode=0;profile-level-id=42001f
 a=ssrc-group:FID 2541098696 3215547008
 a=ssrc:2541098696 cname:cYhx/N8U7h7+3GW3
 a=ssrc:2541098696 msid:SdWLKyaNRoUSWQ7BzkKGcbCWcuV7rScYxCAv e9b60276-a415-4a66-8395-28a893918d4c
