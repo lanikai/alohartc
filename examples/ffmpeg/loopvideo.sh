6bdf84747dd4b9b2ac5df764f7c7ca5fa5415d60
Jordan Carlson <jordan@lanikailabs.com>
2019-01-04 00:14:35 -0800

    Add sample scripts for generating H.264 stream from various sources.
    In one terminal, run
      examples/ffmpeg/loopvideo.sh myfavoritevideo.mp4
    or
      examples/ffmpeg/linux-webcam.sh
    This creates a FIFO named "video.264" and starts writing to it.
    
    Then in another terminal, run
      go run github.com/thinkski/webrtc/examples/demo -i video.264

---
+++ examples/ffmpeg/loopvideo.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+#
+# Loop the given source video repeatedly. Play it in real-time to simulate a live stream.
+
+SOURCE=$1
+DEST=${2:-video.264}
+
+if ! [ -p $DEST ]; then
+    rm -f $DEST
+    mkfifo $DEST
+fi
+
+ffmpeg -re -stream_loop -1 -i $SOURCE \
+    -f h264 -profile:v baseline -level 3.1 \
+    -y $DEST
