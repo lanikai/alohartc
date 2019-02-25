e9bf3af3dfd1154c77139413c9fa44f185f73e09
Chris Hiszpanski <chris@lanikailabs.com>
2019-02-24 22:38:17 -0800

    Expand v4l2 example into a capture utility
    Capture utility reads NALU frames and writes them to a file, convenient
    for capturing samples to debug with.

--- examples/v4l2/demo.go
+++ examples/v4l2/demo.go
@@ -1,14 +1,38 @@
+// Reads NALU frames from v4l2 device and writes to a file
+
 package main
 
 import (
+	"flag"
 	"log"
+	"os"
+	"strconv"
 
 	"github.com/lanikai/alohartc/internal/v4l2"
 )
 
+var (
+	flagBitrate    string
+	flagInputFile  string
+	flagNumPackets string
+	flagOutputFile string
+)
+
+func init() {
+	flag.StringVar(&flagBitrate, "b", "2000000", "Bitrate")
+	flag.StringVar(&flagInputFile, "i", "/dev/video0", "Input device")
+	flag.StringVar(&flagNumPackets, "n", "0", "Number of packets to capture")
+	flag.StringVar(&flagOutputFile, "o", "", "Output file")
+}
+
 func main() {
+	flag.Parse()
+
+	// Default output file
+	outputFile := os.Stdout
+
 	// Open
-	dev, err := v4l2.Open("/dev/video0", &v4l2.Config{
+	dev, err := v4l2.Open(flagInputFile, &v4l2.Config{
 		Width:  1280,
 		Height: 720,
 		Format: v4l2.V4L2_PIX_FMT_H264,
@@ -18,19 +42,45 @@ func main() {
 	}
 	defer dev.Close()
 
+	// Set bitrate
+	if bitrate, err := strconv.Atoi(flagBitrate); err != nil {
+		panic(err)
+	} else {
+		dev.SetBitrate(bitrate)
+	}
+
 	// Start
 	if err := dev.Start(); err != nil {
 		panic(err)
 	}
 	defer dev.Stop()
 
-	// Read buffer after buffer
+	// Open file for writing
+	if flagOutputFile != "" {
+		outputFile, err := os.Create(flagOutputFile)
+		if err != nil {
+			panic(err)
+		}
+		defer outputFile.Close()
+	}
+
+	// Get number of packets to read for
+	numPackets, err := strconv.Atoi(flagNumPackets)
+	if err != nil {
+		panic(err)
+	}
+
+	// Read NALU after NALU
 	p := make([]byte, 1000000)
-	for {
-		if n, err := dev.Read(p); err != nil {
+	for i := 0; numPackets == 0 || i < numPackets; i++ {
+		if nr, err := dev.Read(p); err != nil {
 			panic(err)
 		} else {
-			log.Println("Bytes read = ", n)
+			if nw, err := outputFile.Write(p[:nr]); err != nil {
+				panic(err)
+			} else {
+				log.Println("read:", nr, "written:", nw)
+			}
 		}
 	}
 }
