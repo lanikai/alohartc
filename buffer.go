387a6efd4be9a8d04e79a1a4f72bc63f66fe588f
Jordan Carlson <jordan@lanikailabs.com>
2019-06-19 14:52:33 -0700

    Allow Buffer to be closed.
    This is necessary to be able to unblock `Buffer` readers that are
    waiting for the next packet.
    
    Note: The way `Buffer` is currently used it acts much like `io.Pipe`,
    except that it allows packets to be buffered. There's probably a way to
    achieve this without requiring an allocation on every `Write`. See also
    https://github.com/golang/go/issues/28790.

--- buffer.go
+++ buffer.go
@@ -1,6 +1,6 @@
 //////////////////////////////////////////////////////////////////////////////
 //
-// Buffer for MediaSource implementing io.Reader and io.Writer
+// Buffer for MediaSource implementing io.Reader and io.WriteCloser
 //
 // Copyright 2019 Lanikai Labs. All rights reserved.
 //
@@ -8,36 +8,58 @@
 
 package alohartc
 
-import "errors"
+import (
+	"io"
+)
 
 type Buffer struct {
-	ch chan []byte
+	ch   chan []byte
+	dead chan struct{}
 }
 
-// NewBuffer returns a new buffer
 func NewBuffer() *Buffer {
 	return &Buffer{
-		ch: make(chan []byte, 32),
+		ch:   make(chan []byte, 32),
+		dead: make(chan struct{}),
 	}
 }
 
-// Read next frame from the buffer. Blocks until a frame available.
+// Read next frame from the buffer. Blocks until either a frame is available or
+// the buffer is closed.
 func (b *Buffer) Read(p []byte) (int, error) {
-	var err error
-
-	buf := <-b.ch
-	if len(p) < len(buf) {
-		err = errors.New("read buffer too small. tail of frame is lost.")
+	select {
+	case <-b.dead:
+		return 0, io.EOF
+	case buf := <-b.ch:
+		n := copy(p, buf)
+		if n < len(buf) {
+			return n, io.ErrShortBuffer
+		}
+		return n, nil
 	}
-
-	return copy(p, buf), err
 }
 
 // Write frame into buffer. Blocks until able to write to buffer (i.e. a reader
 // is listening and buffer has space).
-func (b *Buffer) Write(p []byte) {
-	buffer := make([]byte, len(p))
-	copy(buffer, p)
+func (b *Buffer) Write(p []byte) (int, error) {
+	// Copy p into a temporary buffer, that we can send across the channel.
+	buf := append([]byte(nil), p...)
 
-	b.ch <- buffer
+	select {
+	case b.ch <- buf:
+		return len(buf), nil
+	case <-b.dead:
+		return 0, io.ErrClosedPipe
+	}
+}
+
+// Close the buffer. Any further reads will return io.EOF. Further writes will
+// panic.
+func (b *Buffer) Close() error {
+	select {
+	case <-b.dead:
+	default:
+		close(b.dead)
+	}
+	return nil
 }
