8e34eed47c24431a1a824c6cb4ff2ae10f4efc81
Chris Hiszpanski <chris@lanikailabs.com>
2020-01-25 01:06:57 -0800

    Add debian package build targets
    * Support Google Coral, Raspberry Pi 0, Raspberry Pi 3/3B+, Nvidia Tegra
    * Use systemd to start/stop

--- cmd/alohartcd/version.sh
+++ cmd/alohartcd/version.sh
@@ -25,6 +25,26 @@ installedsize=2048
 priority=optional
 homepage=https://alohartc.com
 
+# Generate debian control file for Google Coral EdgeTPU devices
+mkdir -p deb/alohartcd-coral/DEBIAN
+cat >deb/alohartcd-coral/DEBIAN/control <<EOL
+Package: ${package}-coral
+Package-Type: ${packagetype}
+Version: ${version}
+Maintainer: Lanikai Labs LLC <aloha@lanikailabs.com>
+Description: AlohaRTC daemon for Google Coral EdgeTPU devices
+ AlohaRTC is a serverless platform for real-time video communication between
+ connected devices and web as well as mobile apps. This package installs the
+ alohartcd daemon client for Google Coral EdgeTPU devices.
+Section: ${section}
+Priority: ${priority}
+Installed-Size: ${installedsize}
+Architecture: arm64
+Homepage: ${homepage}
+Depends: gstreamer1.0-tools, openssl
+Conflicts: alohartcd-tegra, alohartcd-rpi0, alohartcd-rpi3
+EOL
+
 # Generate debian control file for tegra devices
 mkdir -p deb/alohartcd-tegra/DEBIAN
 cat >deb/alohartcd-tegra/DEBIAN/control <<EOL
@@ -43,5 +63,43 @@ Installed-Size: ${installedsize}
 Architecture: arm64
 Homepage: ${homepage}
 Depends: gstreamer1.0-tools, openssl
-Conflicts: alohartcd-mendel, alohartcd-rpi0, alohartcd-rpi3
+Conflicts: alohartcd-coral, alohartcd-rpi0, alohartcd-rpi3
+EOL
+
+# Generate debian control file for Raspberry Pi 0
+mkdir -p deb/alohartcd-rpi0/DEBIAN
+cat >deb/alohartcd-rpi0/DEBIAN/control <<EOL
+Package: ${package}-rpi0
+Package-Type: ${packagetype}
+Version: ${version}
+Maintainer: Lanikai Labs LLC <aloha@lanikailabs.com>
+Description: AlohaRTC daemon for Raspberry Pi Zero
+ AlohaRTC is a serverless platform for real-time video communication between
+ connected devices and web as well as mobile apps. This package installs the
+ alohartcd daemon client for the Raspberry Pi Zero.
+Section: ${section}
+Priority: ${priority}
+Installed-Size: ${installedsize}
+Architecture: armhf
+Homepage: ${homepage}
+Conflicts: alohartcd-coral, alohartcd-rpi3, alohartcd-tegra
+EOL
+
+# Generate debian control file for Raspberry Pi 3/3B+
+mkdir -p deb/alohartcd-rpi3/DEBIAN
+cat >deb/alohartcd-rpi3/DEBIAN/control <<EOL
+Package: ${package}-rpi3
+Package-Type: ${packagetype}
+Version: ${version}
+Maintainer: Lanikai Labs LLC <aloha@lanikailabs.com>
+Description: AlohaRTC daemon for Raspberry Pi 3/3B+
+ AlohaRTC is a serverless platform for real-time video communication between
+ connected devices and web as well as mobile apps. This package installs the
+ alohartcd daemon client for the Raspberry Pi 3/3B+.
+Section: ${section}
+Priority: ${priority}
+Installed-Size: ${installedsize}
+Architecture: armhf
+Homepage: ${homepage}
+Conflicts: alohartcd-coral, alohartcd-rpi0, alohartcd-rpi3
 EOL
